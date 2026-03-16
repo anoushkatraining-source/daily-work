ddef7231459d33630a2535a4008d18bd9898c05f
anoushkatraining-source <anoushkatraining@gmail.com>
2026-03-16 17:01:14 +0530

    committed

--- Day5/src/com/tek/fi/StreamExample.java
+++ Day5/src/com/tek/fi/StreamExample.java
@@ -6,7 +6,11 @@ public class StreamExample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         stream1();
+        System.out.println("-------------");
         streamWithChain();
+        System.out.println("-------------");
+        //streamOddSquared();
+        comparatorWithAnonymousInnerClass();
 	}
 	private static void stream1() {
 		List<Integer> numbers=Arrays.asList(1,2,3,4,5);
@@ -20,11 +24,31 @@ public class StreamExample {
 	}
 	private static void streamWithChain() {
 		List<Integer> numbers=Arrays.asList(1,2,3,4,5);
-		Stream<Integer> finalstream= numbers.stream().map(number->number*number).filter((number)->{
+		Stream<Integer> finalstream= numbers.stream().parallel().map(number->number*number).filter((number)->{
 			System.out.println(number);
 			return number%2!=0;
 		});
-		System.out.println(finalstream.count());
+		List finalList=finalstream.collect(Collectors.toList());
+		finalList.forEach(x->System.out.println(x));
+		finalList.forEach(System.out::println);
 	}
-
+	
+	
+	/*
+	 * private static void streamOddSquared() { List<Integer>
+	 * numbers=Arrays.asList(1,2,3,4,5); Stream<Integer>
+	 * stream=numbers.stream().map(number->{ return (number %2==1)?
+	 * number*number:number; }).filter((number)->{System.out.println(number); return
+	 * number%2!=0 ;}); System.out.println(stream.count()); }
+	 */
+	private static void comparatorWithAnonymousInnerClass() {
+		List<String> names=Arrays.asList("java","python","GO");
+		names.sort(new Comparator<String>() {
+			public int compare(String o1,String o2) {
+				return o1.length()-o2.length();
+			}
+		});
+		System.out.println(names);
+		
+	}
 }
---
+++ Day5/src/com/tek/fi/StreamProblem.java
@@ -0,0 +1,18 @@
+package com.tek.fi;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StreamProblem {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Stream<Integer> s=Stream.of(1,2,3);
+		s.forEach(System.out::println);
+		s.forEach(System.out::println);
+
+	}
+
+}
