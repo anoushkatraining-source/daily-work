a0674abc37195c183b9ad3365a238b022928314b
anoushkatraining-source <anoushkatraining@gmail.com>
2026-03-31 16:51:13 +0530

    commit

---
+++ notes-api/server.js
@@ -0,0 +1,10 @@
+const express=require('express')
+const app=express();
+const notesRoutes=require("./routes/notesRoutes");
+//console.log('errrrrrr',notesRoutes);
+app.use(express.json());
+app.use('/notes',notesRoutes);
+app.listen(3001,()=>{
+    console.log('server started')
+})
+module.exports=app;
