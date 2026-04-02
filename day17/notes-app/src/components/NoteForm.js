7a94c92dd233a5606d2ee7e9f52b6383cc7910a2
anoushkatraining-source <anoushkatraining@gmail.com>
2026-04-02 15:33:49 +0530

    changed the status fied to radio button

--- day17/notes-app/src/components/NoteForm.js
+++ day17/notes-app/src/components/NoteForm.js
@@ -1,32 +1,43 @@
 import { useState } from "react";
-function NoteForm({addNote}){
-    const[note,setNote]=useState({
-    title:"my note",
-    status: "created"
+function NoteForm({ addNote }) {
+    const [note, setNote] = useState({
+        title: "",
+        status: "open"
     });
-    const handleSubmit=(e)=>{
+    const handleSubmit = (e) => {
         e.preventDefault();
-       // if(!text.trim()) return;
-       // addNote(text);
-        //setText("");
+        if (!note.title.trim()) return;
         addNote(note);
         setNote({
-            title:"",
-            status:""
+            title: "",
+            status: "open"
         });
     };
-    return(
+    return (
         <form onSubmit={handleSubmit}>
-            <input 
-            placeholder="Enter note"
-           // value={text}
-           // onChange={(e)=>setText(e.target.value)}
-            value={note.title}
-            onChange={(e) => setNote({...note,title:e.target.value})}
-         />
-        <input value={note.status} 
-        onChange={(e)=>setNote({...note,status:e.target.value})}/>
-        <button>Add</button>
+            <input
+                placeholder="Enter note"
+                value={note.title}
+                onChange={(e) =>
+                    setNote({ ...note, title: e.target.value })
+                }
+            />
+            <div>
+                <label>
+                    <input
+                        type="checkbox"
+                        checked={note.status === "closed"}
+                        onChange={(event) =>
+                            setNote({
+                                ...note,
+                                status: event.target.checked ? "closed" : "open"
+                            })
+                        }
+                    />
+                    Status
+                </label>
+            </div>
+            <button>Add</button>
         </form>
     );
 }
