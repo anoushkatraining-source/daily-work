bca53ce2fa4784a3c8d97be558c03f6a1d486f37
anoushkatraining-source <anoushkatraining@gmail.com>
2026-04-08 22:27:03 +0530

    done

---
+++ Stage_2_hackathon/easy_push.sh
@@ -0,0 +1,11 @@
+if [ $# -eq 0 ]; then
+    echo "No commit mssage provided. Please provide one."
+else
+	echo "===================================================================\n\n"
+	git add .
+	git commit -m "$1"
+	git push -u origin master
+	echo "\n==================================================================="
+fi
+ 
+echo "Exiting Script...."
